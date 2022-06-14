4a4b57ab57449f5d39b42c8f498fb8fe6977b756
Sigabiel <Sigabiel123@gmail.com>
2022-06-14 14:39:44 +0200

    timeline trash

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,9 +1,16 @@
 import { NextPage } from "next";
+import styles from "../styles/components/Timeline.module.css";
 
 const Timeline : NextPage = () => {
-    return <section>
-    
-  </section>
+    return <div id="project-devlopment" className="container">
+      <div className={styles.timelineBox}>
+        <span className={styles.timelinePoint}></span>
+        <span className={styles.timelinePoint}></span>
+        <span className={styles.timelinePoint}></span>
+        <span className={styles.timelinePoint}></span>
+      </div>
+
+    </div>
   
 }
  // <div className="dropdown animated bounceIn" style="left: -88px;">
